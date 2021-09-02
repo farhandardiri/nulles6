b309f07ceae5d2e995f751d5bf2c64b64be2d4fb
farhandardiri <faran.f4124n@gmail.com>
2021-09-02 09:31:38 +0700

    contoh class

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('oi');
+const data = document.getElementById('data')
+
+
+class Siswa {
+    constructor(username, password, namaKelas) {
+        this.username = username;
+        this.password = password;
+        this.namaKelas = namaKelas;
+    }
+
+    gabung() {
+        console.log(this.username + " Telah bergabung pada kelas " + this.namaKelas);
+    }
+}
+
+let tambahData = new Siswa('farhan', '1234', 'Fiqh');
+tambahData.gabung()
