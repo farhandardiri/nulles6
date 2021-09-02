6031b157e15e3a43b030f0bf62e873dd7d7f36bc
farhandardiri <faran.f4124n@gmail.com>
2021-09-02 10:05:08 +0700

    extens kelas

--- main.js
+++ main.js
@@ -11,7 +11,24 @@ class Siswa {
     gabung() {
         console.log(this.username + " Telah bergabung pada kelas " + this.namaKelas);
     }
+    member() {
+        console.log('100 member');
+    }
 }
 
-let tambahData = new Siswa('farhan', '1234', 'Fiqh');
-tambahData.gabung()
+// let tambahData = new Siswa('farhan', '1234', 'Fiqh');
+// tambahData.gabung()
+
+class Lagganan extends Siswa {
+    constructor(username, paket) {
+        super(username);
+        this.paket = paket;
+    }
+
+    gabungPaket() {
+        console.log('Hi ' + this.username + ' telah bergabung paket ' + this.paket);
+    }
+}
+
+let tambahLangganan = new Lagganan('farhan', 'premium');
+tambahLangganan.member()
